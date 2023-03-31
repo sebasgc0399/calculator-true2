9fdde47580c0b84644cc8aed290dccf88c27861e
Sebastian <sebasgc0399@gmail.com>
2023-03-31 18:56:14 -0500

    Funciona

--- Backend/app.py
+++ Backend/app.py
@@ -1,20 +1,16 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from starlette.requests import Request
-from starlette.middleware.cors import CORSMiddleware as StarletteCORSMiddleware
 from classes.operation import Operation
 
-from sympy import *
-from sympy.parsing.sympy_parser import parse_expr
-import pandas as pd
-import regex as re
+import itertools
+import re
+
+from sympy import symbols, sympify
+from sympy.logic.boolalg import Not, And, Or, Nand, Nor, Xor
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application
 
 
-from sympy.logic.boolalg import BooleanFunction
-from sympy.parsing.sympy_parser import (parse_expr, standard_transformations, implicit_multiplication_application)
-from sympy import sympify, SympifyError
 app = FastAPI()
 
 origins = [
@@ -29,172 +25,147 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-"""# Definir las funciones personalizadas
-class Nand(BooleanFunction):
-    @classmethod
-    def eval(cls, *args):
-        result = And(*args)
-        if result is not None:
-            return Not(result)
-
-class Nor(BooleanFunction):
-    @classmethod
-    def eval(cls, *args):
-        result = Or(*args)
-        if result is not None:
-            return Not(result)
-
-class Xor(BooleanFunction):
-    @classmethod
-    def eval(cls, *args):
-        if len(args) != 2:
-            raise ValueError("Xor requiere exactamente dos argumentos")
-        a, b = args
-        return Or(And(a, Not(b)), And(Not(a), b))"""
+def verificar_sintaxis(expresion):
+    stack = []
+    for char in expresion:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
 
 
-# Define los operadores lógicos permitidos
-# logical_operators = {'and', 'or', 'not', 'implies', 'iff'}
-logical_operators = {'∧', '∨', '¬', '⊼', '⊽', '⊕'}
-# Función para verificar la sintaxis de la proposición
-
-
-def is_well_formed(proposition):
-    # Elimina todos los espacios en blanco y paréntesis
-    proposition = ''.join(proposition.split())
-    proposition = proposition.replace('(', '')
-    proposition = proposition.replace(')', '')
-
-    # Comprueba que solo se usen operadores lógicos permitidos
-    words = proposition.split()
-
-    # Comprueba que no haya dos operadores consecutivos
-    for i in range(len(words) - 1):
-        if words[i] in logical_operators and words[i + 1] in logical_operators:
-            return f"Hay dos operadores lógicos consecutivos: '{words[i]}' y '{words[i+1]}'"
-
-    # Comprueba que la proposición no termine con un operador lógico
-    if words[-1] in logical_operators:
-        return f"La proposición no puede terminar con el operador lógico '{words[-1]}'"
-
-    return True
-
-
-# Función para convertir la proposición en una expresión de Sympy
-def to_sympy_expression(proposition):
-    # Reemplaza los operadores lógicos por sus equivalentes en Sympy
-    proposition = proposition.replace('∧', '&')
-    proposition = proposition.replace('∨', '|')
-    proposition = proposition.replace('¬', '~')
-    proposition = proposition.replace('⊕', '^')
-
-    # Define las funciones lambda para NAND y NOR
-    nand = lambda a, b: Not(And(a, b))
-    nor = lambda a, b: Not(Or(a, b))
-
-    # Reemplaza los operadores NAND y NOR en la proposición
-    while '⊼' in proposition or '⊽' in proposition:
-        for op, func in [('⊼', nand), ('⊽', nor)]:
-            i = proposition.find(op)
-            if i != -1:
-                left = symbols(proposition[i - 1])
-                right = symbols(proposition[i + 1])
-                new_expr = func(left, right)
-                proposition = proposition[:i - 1] + str(new_expr) + proposition[i + 2:]
-
-    try:
-        expr = parse_expr(proposition, evaluate=False)
-        return expr
-    except SympifyError as e:
-        error_type = type(e).__name__
-        error_location = str(e).split('\'')[1]
-        raise SympifyError(f"Error de sintaxis en la proposición: {error_type} en {error_location}")
-
-
-def revert_operators(expression):
-    expression = expression.replace('&', '∧')
-    expression = expression.replace('|', '∨')
-    expression = expression.replace('~', '¬')
-    expression = expression.replace('~&', '⊼')
-    expression = expression.replace('~|', '⊽')
-    expression = expression.replace('^', '⊕')
-
-    # Reemplaza las expresiones equivalentes de NAND y NOR por sus símbolos originales
-    expression = expression.replace('~&', '⊼')
-    expression = expression.replace('~|', '⊽')
+def convertir_sympy(expresion):
+    # Define las funciones lambda para NAND, NOR y XOR
+    nand = lambda a, b: ~(a & b)
+    nor = lambda a, b: ~(a | b)
+    xor = lambda a, b: a ^ b
     
-    return expression
+    # Obtiene las variables en la expresión
+    variables = sorted(list(set(re.findall(r'[A-Za-z]', expresion))))
+    simbolos = symbols(" ".join(variables))
+    simbolo_dict = {str(var): var for var in simbolos}
+    
+    # Define una función para aplicar la función de operador adecuada a las variables
+    def apply_operator(op, a, b):
+        if op == '∧':
+            return a & b
+        elif op == '∨':
+            return a | b
+        elif op == '¬':
+            return ~b
+        elif op == '⊕':
+            return xor(a, b)
+        elif op == '⊼':
+            return nand(a, b)
+        elif op == '⊽':
+            return nor(a, b)
+
+    # Define una función recursiva para analizar y construir la expresión Sympy
+    def parse_recursive(exp):
+        stack = []
+        i = 0
+        while i < len(exp):
+            char = exp[i]
+            if char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                stack.append(simbolo_dict[char])
+            elif char in '∧∨⊕⊼⊽':
+                if len(stack) >= 2:
+                    b = stack.pop()
+                    a = stack.pop()
+                    stack.append(apply_operator(char, a, b))
+                else:
+                    stack.append(char)
+            elif char == '¬':
+                if len(stack) >= 1:
+                    stack.append(apply_operator(char, None, stack.pop()))
+                else:
+                    stack.append(char)
+            elif char == '(':
+                j = i
+                open_parens = 1
+                while open_parens > 0:
+                    j += 1
+                    if exp[j] == '(':
+                        open_parens += 1
+                    elif exp[j] == ')':
+                        open_parens -= 1
+                inner_expr = exp[i + 1:j]
+                stack.append(parse_recursive(inner_expr))
+                i = j
+            i += 1
+
+        while len(stack) > 1:
+            b = stack.pop()
+            op = stack.pop()
+            a = stack.pop()
+            stack.append(apply_operator(op, a, b))
+
+        return stack[0]
+
+    # Llama a la función recursiva para construir la expresión Sympy
+    sympy_expresion = parse_recursive(expresion)
+    
+    return sympy_expresion
 
 
-def realizar_calculo(proposicion):
-    check = is_well_formed(proposicion)
-    if check is True:
-        expresion = to_sympy_expression(proposicion)
-        if expresion is not None:
-            return expresion
+
+
+def revertir_operadores(expresion_sympy):
+    expresion = str(expresion_sympy)
+    expresion = expresion.replace("~", "¬").replace("&", "∧").replace("|", "∨")
+    expresion = expresion.replace("Xor", "⊕").replace("Nand", "⊼").replace("Nor", "⊽")
+    return expresion
+
+def generar_tabla_de_verdad(expresion):
+    if not verificar_sintaxis(expresion):
+        raise ValueError("Sintaxis de la proposición incorrecta")
+
+    variables = sorted(list(set(re.findall(r'[A-Za-z]', expresion))))
+    simbolos = symbols(" ".join(variables))
+    #simbolo_dict = {str(var): var for var in simbolos}
+    formula = convertir_sympy(expresion)
+    print(formula)
+    encabezado = variables + [expresion]
+    tabla = [encabezado]
+
+    for valores in itertools.product([False, True], repeat=len(variables)):
+        asignacion = dict(zip(simbolos, valores))
+        fila = [valor for valor in valores]
+
+        if not isinstance(formula, bool):
+            print("Expresión antes de subs:", formula)
+            fila.append(formula.subs(asignacion))
         else:
-            print('Error al convertir la proposición a una expresión de Sympy')
-    else:
-        print(check)
+            fila += [formula]
 
-# Función para obtener los valores de verdad de una proposición
-def obtener_valores_de_verdad(proposicion):
-    # Parsear la proposición como una expresión simbólica
-    expr = to_sympy_expression(proposicion)
-    # Obtener las variables de la proposición
-    variables = list(expr.free_symbols)
-    # Crear una tabla de verdad completa
-    tabla = []
-    # Crear una lista para almacenar las columnas de operaciones lógicas
-    columnas_operaciones = []
-    for i in range(2**len(variables)):
-        fila = []
-        for j, variable in enumerate(variables):
-            valor_variable = (i // 2**j) % 2
-            fila.append(True if valor_variable == 1 else False)
-        # Evaluamos cada operación lógica y añadimos su resultado como una columna adicional
-        for operacion in expr.atoms(And, Or, Not, Xor, Nand, Nor):
-            resultado_operacion = operacion.subs(
-                {variables[k]: (i // 2**k) % 2 for k in range(len(variables))})
-            nombre_operacion = str(operacion)
-            if nombre_operacion not in columnas_operaciones:
-                columnas_operaciones.append(nombre_operacion)
-            fila.append(str(bool(resultado_operacion)))
-        tabla.append(fila)
+        tabla.append([("V" if valor else "F") for valor in fila[:-1]] + [("V" if fila[-1] else "F")])
+    
+    for fila in tabla:
+        print(" | ".join(fila))
 
-    # Agregar las columnas de operaciones lógicas al conjunto de columnas
-    columnas = [str(var) for var in variables] + columnas_operaciones
+    return tabla
 
-    # Ordenar las columnas de menor a mayor longitud
-    columnas_ordenadas = sorted(columnas, key=lambda x: len(str(x)))
-
-    # Crear un DataFrame de pandas con la tabla de verdad
-    df = pd.DataFrame(tabla, columns=columnas_ordenadas)
-
-    # Agregar una columna con los valores finales de la proposición
-    df[proposicion] = df.apply(lambda row: str(
-        bool(expr.subs({variables[k]: row[k] for k in range(len(variables))}))), axis=1)
-
-    matriz_de_listas = [df.columns.tolist()] + df.values.tolist()
-
-    # Aplicar la función revert_operators a cada elemento de la matriz de listas
-    matriz_de_listas_revertida = [[revert_operators(str(item)) for item in row] for row in matriz_de_listas]
-
-    return (matriz_de_listas_revertida)
+expresion = "A⊽B"
+tabla_de_verdad = generar_tabla_de_verdad(expresion)
+# La variable "tabla_de_verdad" esta la matriz
+print("Probando")
 
 
 @app.get("/")
 def read_root():
     return {"response": "Welcome to use free Algebra boolean calculator"}
 
-
+#Corregir
 @app.post('/formula')
 def procesar_proposicion(operation: Operation):
     proposicion = operation.formula
-    expresion = realizar_calculo(proposicion)
+    expresion = proposicion
     print(expresion)
     if expresion is not None:
-        response = obtener_valores_de_verdad(str(expresion))
+        response = generar_tabla_de_verdad(str(expresion))
         print(response)
         return {"response": response}
     else:
